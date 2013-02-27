7d7e40bfd32c7aaa0dd5e687bafd98213504dcd2
Sergey Ponomarev <stokito@gmail.com>
2013-02-27 03:43:17 +0200

    Reformat code

--- src/main/java/org/kranonit/unitTestExample/calculator/Calculator.java
+++ src/main/java/org/kranonit/unitTestExample/calculator/Calculator.java
@@ -1,16 +1,21 @@
 package org.kranonit.unitTestExample.calculator;
 
 public class Calculator {
-    public int sum(int a, int b) {
-        return a + b;
-    }
-    public int minus(int a, int b) {
-        return a + b;  // ERROR!!!
-    }
-    public int divide(int a, int b) {
-        return a / b;
-    }
-    public int multiply(int a, int b) {
-        return a * b;
-    }
+
+	public int sum(int a, int b) {
+		return a + b;
+	}
+
+	public int minus(int a, int b) {
+		return a + b;  // ERROR!!!
+	}
+
+	public int divide(int a, int b) {
+		return a / b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
 }
--- src/test/java/org/kranonit/unitTestExample/calculator/CalculatorTest.java
+++ src/test/java/org/kranonit/unitTestExample/calculator/CalculatorTest.java
@@ -4,28 +4,32 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 public class CalculatorTest {
-    @Test public void testSum() {
+
+	@Test
+	public void testSum() {
 		// Given
-        Calculator calculator = new Calculator();
+		Calculator calculator = new Calculator();
 		// When
-        int result = calculator.sum(2, 2);
+		int result = calculator.sum(2, 2);
 		// Then
-        Assert.assertTrue(result == 4);
-    }
+		Assert.assertTrue(result == 4);
+	}
 
-    @Test public void testMinus() {
-        Calculator calculator = new Calculator();
-        Assert.assertEquals(0, calculator.minus(2, 2));
-    }
+	@Test
+	public void testMinus() {
+		Calculator calculator = new Calculator();
+		Assert.assertEquals(0, calculator.minus(2, 2));
+	}
 
-    @Test public void testDivide() {
-        Calculator calculator = new Calculator();
-        Assert.assertEquals(2, calculator.divide(6, 3));
-    }
+	@Test
+	public void testDivide() {
+		Calculator calculator = new Calculator();
+		Assert.assertEquals(2, calculator.divide(6, 3));
+	}
 
-    @Test(expected = ArithmeticException.class)
-    public void testDivideWillThrowExceptionWhenDivideOnZero() {
-        Calculator calculator = new Calculator();
-        calculator.divide(6, 0);
-    }
+	@Test(expected = ArithmeticException.class)
+	public void testDivideWillThrowExceptionWhenDivideOnZero() {
+		Calculator calculator = new Calculator();
+		calculator.divide(6, 0);
+	}
 }
